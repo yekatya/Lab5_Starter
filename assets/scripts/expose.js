ede6f095e5291f86cb1826f1acafc96980ccc336
Yekaterina Ruslanovna Kostina <47643195+yekatya@users.noreply.github.com>
2021-11-09 15:13:02 -0800

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -15,17 +15,17 @@ function hornFunc() {
   var noImg = document.querySelector("img");
   var audioElem = document.querySelector("audio");
   if (document.getElementById("horn-select").value == "air-horn") {
-    noImg.src = "/assets/images/air-horn.svg";
+    noImg.src = "assets/images/air-horn.svg";
     noImg.alt = "Air Horn Image";
-    audioElem.src = "/assets/audio/air-horn.mp3";
+    audioElem.src = "assets/audio/air-horn.mp3";
   } else if (document.getElementById("horn-select").value == "car-horn") {
-    noImg.src = "/assets/images/car-horn.svg";
+    noImg.src = "assets/images/car-horn.svg";
     noImg.alt = "Car Horn Image";
-    audioElem.src = "/assets/audio/car-horn.mp3";
+    audioElem.src = "assets/audio/car-horn.mp3";
   } else {
-    noImg.src = "/assets/images/party-horn.svg";
+    noImg.src = "assets/images/party-horn.svg";
     noImg.alt = "Party Horn Image";
-    audioElem.src = "/assets/audio/party-horn.mp3";
+    audioElem.src = "assets/audio/party-horn.mp3";
   }
 }
 
@@ -35,22 +35,22 @@ function volumeFunc() {
   var volumeImg = divElement.querySelector("img");
   var audioElem = document.querySelector("audio");
   if (volElement.value == "0") {
-    volumeImg.src = "/assets/icons/volume-level-0.svg";
+    volumeImg.src = "assets/icons/volume-level-0.svg";
     volumeImg.alt = "Volume level 0";
     audioElem.volume = "0";
   }
   if (volElement.value > 1 && volElement.value < 33) {
-    volumeImg.src = "/assets/icons/volume-level-1.svg";
+    volumeImg.src = "assets/icons/volume-level-1.svg";
     volumeImg.alt = "Volume level 1";
     audioElem.volume = "0.33";
   }
   if (volElement.value >= 33 && volElement.value < 67) {
-    volumeImg.src = "/assets/icons/volume-level-2.svg";
+    volumeImg.src = "assets/icons/volume-level-2.svg";
     volumeImg.alt = "Volume level 2";
     audioElem.volume = "0.67";
   }
   if (volElement.value >= 67) {
-    volumeImg.src = "/assets/icons/volume-level-3.svg";
+    volumeImg.src = "assets/icons/volume-level-3.svg";
     volumeImg.alt = "Volume level 3";
     audioElem.volume = "1";
   }
